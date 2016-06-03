f633c059775c44d6260dded6d0fefbab64cdd22a
Usama Noman <usama.noman@outlook.com>
2016-06-03 12:23:26 +0500

    Uploading whole plugin

---
+++ featured-images/better-rest-api-featured-images.php
@@ -0,0 +1,110 @@
+<?php
+
+add_action( 'plugins_loaded', 'better_rest_api_featured_images_load_translations' );
+/**
+ * Load translation files.
+ *
+ * @since  1.2.0
+ */
+function botsify_rest_api_featured_images_load_translations() {
+    load_plugin_textdomain( 'better-rest-api-featured-images', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+
+add_action( 'init', 'better_rest_api_featured_images_init', 12 );
+/**
+ * Register our enhanced botsify_featured_image field to all public post types
+ * that support post thumbnails.
+ *
+ * @since  1.0.0
+ */
+function botsify_rest_api_featured_images_init() {
+
+	$post_types = get_post_types( array( 'public' => true ), 'objects' );
+
+	foreach ( $post_types as $post_type ) {
+
+		$post_type_name     = $post_type->name;
+		$show_in_rest       = ( isset( $post_type->show_in_rest ) && $post_type->show_in_rest ) ? true : false;
+		$supports_thumbnail = post_type_supports( $post_type_name, 'thumbnail' );
+
+		// Only proceed if the post type is set to be accessible over the REST API
+		// and supports featured images.
+		if ( $show_in_rest && $supports_thumbnail ) {
+
+			// Compatibility with the REST API v2 beta 9+
+			if ( function_exists( 'register_rest_field' ) ) {
+				register_rest_field( $post_type_name,
+					'botsify_featured_image',
+					array(
+						'get_callback' => 'botsify_rest_api_featured_images_get_field',
+						'schema'       => null,
+					)
+				);
+			} elseif ( function_exists( 'register_api_field' ) ) {
+				register_api_field( $post_type_name,
+					'botsify_featured_image',
+					array(
+						'get_callback' => 'botsify_rest_api_featured_images_get_field',
+						'schema'       => null,
+					)
+				);
+			}
+		}
+	}
+}
+
+/**
+ * Return the botsify_featured_image field.
+ *
+ * @since   1.0.0
+ *
+ * @param   object  $object      The response object.
+ * @param   string  $field_name  The name of the field to add.
+ * @param   object  $request     The WP_REST_Request object.
+ *
+ * @return  object|null
+ */
+function botsify_rest_api_featured_images_get_field( $object, $field_name, $request ) {
+
+	// Only proceed if the post has a featured image.
+	if ( ! empty( $object['featured_media'] ) ) {
+		$image_id = (int)$object['featured_media'];
+	} elseif ( ! empty( $object['featured_image'] ) ) {
+		$image_id = (int)$object['featured_image'];
+	} else {
+		return null;
+	}
+
+	$image = get_post( $image_id );
+
+	if ( ! $image ) {
+		return null;
+	}
+
+	// This is taken from WP_REST_Attachments_Controller::prepare_item_for_response().
+	$featured_image['id']            = $image_id;
+	$featured_image['alt_text']      = get_post_meta( $image_id, '_wp_attachment_image_alt', true );
+	$featured_image['caption']       = $image->post_excerpt;
+	$featured_image['description']   = $image->post_content;
+	$featured_image['media_type']    = wp_attachment_is_image( $image_id ) ? 'image' : 'file';
+	$featured_image['media_details'] = wp_get_attachment_metadata( $image_id );
+	$featured_image['post']          = ! empty( $image->post_parent ) ? (int) $image->post_parent : null;
+	$featured_image['source_url']    = wp_get_attachment_url( $image_id );
+
+	if ( empty( $featured_image['media_details'] ) ) {
+		$featured_image['media_details'] = new stdClass;
+	} elseif ( ! empty( $featured_image['media_details']['sizes'] ) ) {
+		$img_url_basename = wp_basename( $featured_image['source_url'] );
+		foreach ( $featured_image['media_details']['sizes'] as $size => &$size_data ) {
+			$image_src = wp_get_attachment_image_src( $image_id, $size );
+			if ( ! $image_src ) {
+				continue;
+			}
+			$size_data['source_url'] = $image_src[0];
+		}
+	} else {
+		$featured_image['media_details']['sizes'] = new stdClass;
+	}
+
+	return apply_filters( 'better_rest_api_featured_image', $featured_image, $image_id );
+}
